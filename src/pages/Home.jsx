987d5e23bcf327a2920cbb2ae211e7467d6aad23
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-08 03:35:12 -0700

    feat: import PhotoWindow component with Try/CATCH 2024 banner image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import PhotoWindow from "../components/PhotoWindow";
+
+import banner from "../assets/images/Home/banner.png"
+
 function Home(){
     return(
         <div>
-            Home
+            {/* Hero section */}
+            <div>
+                <PhotoWindow img={banner} alt="Group photo of Try/CATCH 2024 attendees at the SFU ASB Atrium" />
+            </div>
         </div>
     )
 }
