1cd8fa965026ae3be186adb73fdbb48bc1b1c4c5
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-07 22:45:22 -0700

    feat: set up react router with home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
