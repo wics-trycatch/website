742fde87f40c214ba7de58a8a0fb4901aa50f8d1
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-08 23:23:29 -0700

    feat: add imgPosition prop with documentation

--- src/components/PhotoWindow.jsx
+++ src/components/PhotoWindow.jsx
@@ -5,6 +5,7 @@
  * @param {string} imgWidth - Tailwind width class (e.g., `"w-[50%]"`, `"w-64"`). 
  *                            Pass the full class string, not just the value.
  * @param {string} imgHeight - Tailwind height class (e.g., `"h-[50%]"`, `"h-64"`).
+ * @param {string} imgPosition - Tailwind object position class (e.g., `"object-center"`).
  * @param {string} alt - Accessible alt text describing the image.
  * 
  * @example
@@ -13,13 +14,14 @@
  *   img={banner}
  *   imgWidth="w-[50%]"
  *   imgHeight="h-[50%]"
+ *   imgPosition="object-center"
  *   alt="Group photo of Try/CATCH 2024 attendees"
  * />
  */
-function PhotoWindow({ img, imgWidth, imgHeight, alt }) {
+function PhotoWindow({ img, imgWidth, imgHeight, imgPosition, alt }) {
     return (
         <div>
-            <img src={img} alt={alt} className={`${imgWidth} ${imgHeight} object-cover`} />
+            <img src={img} alt={alt} className={`${imgWidth} ${imgHeight} ${imgPosition} object-cover`} />
         </div>
     );
 }
