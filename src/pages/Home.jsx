cb04839858b3148d78d5caf372bdc868414064ea
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-09 21:54:37 -0700

    style: add styling for hero section logo and description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
+import styles from "./Home.module.css";
+
 import PhotoWindow from "../components/PhotoWindow";
 
-import banner from "../assets/images/Home/banner.png"
+import banner from "../assets/images/home/banner.png"
+import logo from "../assets/images/shared/logo_full.png";
 
 function Home(){
     return(
         <div>
             {/* Hero section */}
-            <div>
-                <PhotoWindow img={banner} alt="Group photo of Try/CATCH 2024 attendees at the SFU ASB Atrium" />
+            <div className={`relative`}>
+                <div className={`absolute z-100 top-[13rem] left-[3rem]`}>
+                    <img src={logo} alt="Try/CATCH Logo" className={`w-[90%] ml-[-0.5rem]`} />
+                    <p className={`${styles.heroDesc} max-w-[18rem] mt-[1rem] ml-[1.75rem]`}>
+                        A tech conference for high school girls and non-binary students to learn, connect, and get inspired.
+                    </p>
+                </div>
+                <div className={`scale-90`}>
+                    <PhotoWindow img={banner} imgWidth="w-[100%]" imgHeight="h-[92vh]" imgPosition="object-[55%_15%]" windowBorder="border-[8px]" alt="Group photo of Try/CATCH 2024 attendees at the SFU ASB Atrium" />
+                </div>
             </div>
         </div>
     )
