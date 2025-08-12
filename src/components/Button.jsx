897b3867c8e48c4bb21190ae5a2e9821076a584f
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-11 21:18:27 -0700

    style: add hover state and navbar type, fix variable styles for hero, navbar, and default styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,12 @@ function Button({ link, text, bgColor, textColor, type }) {
   return (
     <Link
       to={link}
-      className={`font-body-bold bg-light-cyan ${bgColor} text-dark-blue ${textColor} px-[1.5rem] py-[0.5rem] border-[3.5px] 2xl:px-[3rem] 2xl:py-[1rem] 2xl:border-[6px] ${
+      className={`font-body-bold bg-light-cyan hover:bg-dark-blue hover:rounded-md ${bgColor} text-dark-blue hover:text-powder-blue ${textColor} px-[1.5rem] py-[0.5rem] 2xl:px-[3rem] 2xl:py-[1rem] transition-all duration-300 ease-in-out ${
         type === "hero"
-          ? "text-[1.25rem] 2xl:text-[2rem]"
-          : "text-[1rem] 2xl:text-[1.5rem]"
+          ? "text-[1.25rem] 2xl:text-[2rem] border-dark-blue border-[3.5px] 2xl:border-[6px]" 
+          : type === "navbar"
+          ? "text-[1rem] 2xl:text-[1.5rem] bg-white border-0 2xl:border-0"
+          : "text-[1rem] 2xl:text-[1.5rem] border-dark-blue border-[3.5px] 2xl:border-[6px]"
       }`}
     >
       {text}
