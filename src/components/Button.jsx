e76b7f0ea9509d9d533aba5a24ecc651d92b5f85
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-11 20:15:01 -0700

    refactor: make component handle responsiveness for padding and font size, add type prop for hero buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 
-function Button({link, text, bgColor, textColor, size, fontSize}){
+function Button({link, text, bgColor, textColor, type}){
     return(
-        <Link to={link} className={`font-body-bold bg-light-cyan ${bgColor} text-dark-blue ${textColor} ${size === "small" ? "px-[1.5rem] py-[0.5rem] border-[3.5px]" : size === "large" ? "px-[3rem] py-[1rem] border-[6px]" : ""} ${fontSize}`}>
+        <Link to={link} className={`font-body-bold bg-light-cyan ${bgColor} text-dark-blue ${textColor} px-[1.5rem] py-[0.5rem] border-[3.5px] 2xl:px-[3rem] 2xl:py-[1rem] 2xl:border-[6px] ${type === "hero" ? "text-[1.25rem] 2xl:text-[2rem]" : "text-[1rem] 2xl:text-[1.5rem]"}`}>
             {text}
         </Link>
 
