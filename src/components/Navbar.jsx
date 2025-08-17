20492eb8df0c4bc5578c7e5d13d7a2c0e47e9f3c
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-17 02:09:13 -0700

    style: add styling to navbar items and add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import Button from './Button';
+import styles from "./Navbar.module.css";
+import Button from "./Button";
+
+import logo from "../assets/images/shared/logo_simple_white.png"
 
 function Navbar(){
     return(
-        <nav>
-            <ul>
-                <li>Try/CATCH</li>
-                <li>EVENT DETAILS</li>
+        <nav className={`mx-[5%] px-[2rem] py-[1.5rem] flex justify-between bg-dark-blue`}>
+            <img src={logo} alt="Logo" className="h-[3rem] w-[auto]" />
+            <ul className={`flex gap-[3rem]`}>
+                <li>EVENT DETAILS
+                    <ul>
+                        <li className={`hidden`}>SCHEDULE</li>
+                        <li className={`hidden`}>WORKSHOPS</li>
+                    </ul>
+                </li>
                 <li>SPONSORS</li>
                 <li>FAQ</li>
                 <li>SFU WICS</li>
