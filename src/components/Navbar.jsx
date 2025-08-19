721574011ad60f5473fcaf51df9ebfe79370cd2e
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-19 14:27:15 -0700

    style: add styles to navbar to match design more closely

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import Button from "./Button";
 
 import logo from "../assets/images/shared/logo_simple_white.png"
 
+import { ChevronDown } from 'lucide-react';
+
 function Navbar(){
     return(
         <nav className={`mx-[5%] px-[2.5rem] pt-[2rem] pb-[0.5rem] flex justify-between bg-dark-blue`}>
             <img src={logo} alt="Homepage" className="h-[3rem] w-[auto] -translate-y-2" />
             <ul role="menu" aria-label="Main menu" className={`${styles.navList} flex gap-[3rem]`}>
-                <li role="menuitem" tabIndex="0" className={`${styles.dropdown} z-100`}>EVENT DETAILS
+                <li role="menuitem" tabIndex="0" className={`${styles.dropdown} z-100`}><div className={`flex`}><span className={`mr-[0.5rem]`}>EVENT DETAILS</span><ChevronDown size={36} /></div>
                     <ul role="menu" className={`${styles.dropdownContent}`}>
                         <li role="menuitem" tabIndex="0">SCHEDULE</li>
                         <li role="menuitem" tabIndex="0">WORKSHOPS</li>
