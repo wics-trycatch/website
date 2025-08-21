a0449b010ebe315ea511ffa07f0237c720b57d66
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-21 11:13:17 -0700

    style: fix hover state bg color for navbar Button type

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,12 @@ function Button({ link, text, bgColor, textColor, type }) {
   return (
     <Link
       to={link}
-      className={`font-body-bold bg-light-cyan hover:bg-dark-blue hover:rounded-md ${bgColor} text-dark-blue hover:text-powder-blue ${textColor} px-[1.5rem] py-[0.5rem] 2xl:py-[1rem] transition-all duration-300 ease-in-out ${
+      className={`font-body-bold bg-light-cyan  hover:rounded-md ${bgColor} text-dark-blue ${textColor} px-[1.5rem] py-[0.5rem] 2xl:py-[1rem] transition-all duration-300 ease-in-out ${
         type === "hero"
-          ? "text-[1.25rem] 2xl:text-[2rem] border-dark-blue border-[3.5px] 2xl:border-[6px] 2xl:px-[3rem]" 
+          ? "text-[1.25rem] 2xl:text-[2rem] hover:bg-dark-blue border-dark-blue hover:text-powder-blue border-[3.5px] 2xl:border-[6px] 2xl:px-[3rem]" 
           : type === "navbar"
-          ? "text-[1rem] 2xl:text-[1.5rem] bg-white border-0 2xl:border-0 2xl:px-[2rem]"
-          : "text-[1rem] 2xl:text-[1.5rem] border-dark-blue border-[3.5px] 2xl:border-[6px] 2xl:px-[3rem]"
+          ? "text-[1rem] 2xl:text-[1.5rem] bg-white hover:bg-powder-blue hover:text-dark-blue border-0 2xl:border-0 2xl:px-[2rem]"
+          : "text-[1rem] 2xl:text-[1.5rem] hover:bg-dark-blue border-dark-blue hover:text-powder-blue border-[3.5px] 2xl:border-[6px] 2xl:px-[3rem]"
       }`}
     >
       {text}
