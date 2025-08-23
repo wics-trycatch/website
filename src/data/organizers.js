c0fa112deac7785adb1dd5c5b5b634adf2f6101c
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-23 13:30:07 -0700

    feat: add organizers array to easily add organizers to the SFU WiCS page

---
+++ src/data/organizers.js
@@ -0,0 +1,173 @@
+import amelia from "../assets/images/wics/placeholder_headshot.png";
+import aniyah from "../assets/images/wics/placeholder_headshot.png";
+import bhakti from "../assets/images/wics/placeholder_headshot.png";
+import bianca from "../assets/images/wics/placeholder_headshot.png";
+import emily from "../assets/images/wics/placeholder_headshot.png";
+import gloria from "../assets/images/wics/placeholder_headshot.png";
+import grace from "../assets/images/wics/placeholder_headshot.png";
+import mara from "../assets/images/wics/placeholder_headshot.png";
+import mariaF from "../assets/images/wics/placeholder_headshot.png";
+import mariaK from "../assets/images/wics/placeholder_headshot.png";
+import meyer from "../assets/images/wics/placeholder_headshot.png";
+import poorvi from "../assets/images/wics/placeholder_headshot.png";
+import sayna from "../assets/images/wics/placeholder_headshot.png";
+import selina from "../assets/images/wics/placeholder_headshot.png";
+import serena from "../assets/images/wics/placeholder_headshot.png";
+import sydney from "../assets/images/wics/placeholder_headshot.png";
+import vidhi from "../assets/images/wics/placeholder_headshot.png";
+
+export const organizers = [
+  {
+    img: mara,
+    imgProperties: "object-center",
+    alt: "Mara headshot",
+    name: "Mara",
+    role: "Co-Chair • Website Developer",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: bhakti,
+    imgProperties: "object-center",
+    alt: "Bhakti headshot",
+    name: "Bhakti",
+    role: "Co-Chair",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: aniyah,
+    imgProperties: "object-center",
+    alt: "Aniyah headshot",
+    name: "Aniyah",
+    role: "Logistics Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: grace,
+    imgProperties: "object-center",
+    alt: "Grace headshot",
+    name: "Grace",
+    role: "Logistics Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: meyer,
+    imgProperties: "object-center",
+    alt: "Meyer headshot",
+    name: "Meyer",
+    role: "Logistics Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: mariaK,
+    imgProperties: "object-center",
+    alt: "Maria K headshot",
+    name: "Maria K.",
+    role: "Sponsorships Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: vidhi,
+    imgProperties: "object-center",
+    alt: "Vidhi headshot",
+    name: "Vidhi",
+    role: "Sponsorships Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: gloria,
+    imgProperties: "object-center",
+    alt: "Gloria headshot",
+    name: "Gloria",
+    role: "Treasurer",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: serena,
+    imgProperties: "object-center",
+    alt: "Serena headshot",
+    name: "Serena",
+    role: "Communications Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: poorvi,
+    imgProperties: "object-center",
+    alt: "Poorvi headshot",
+    name: "Poorvi",
+    role: "Communications Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: sydney,
+    imgProperties: "object-center",
+    alt: "Sydney headshot",
+    name: "Sydney",
+    role: "Logistics Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: emily,
+    imgProperties: "object-center",
+    alt: "Emily headshot",
+    name: "Emily",
+    role: "Logistics Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: selina,
+    imgProperties: "object-center",
+    alt: "Selina headshot",
+    name: "Selina",
+    role: "Visual Design Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: amelia,
+    imgProperties: "object-center",
+    alt: "Amelia headshot",
+    name: "Amelia",
+    role: "Visual Design Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: mariaF,
+    imgProperties: "object-center",
+    alt: "Maria F headshot",
+    name: "Maria F.",
+    role: "Merch & Swag Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: sayna,
+    imgProperties: "object-center",
+    alt: "Sayna headshot",
+    name: "Sayna",
+    role: "Merch & Swag Coordinator",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: bianca,
+    imgProperties: "object-center",
+    alt: "Bianca headshot",
+    name: "Bianca",
+    role: "Website Developer",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+];
