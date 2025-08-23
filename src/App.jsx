32d1c65c8baba6bcdedafbb9e2aa8ccb914b7a5b
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-22 17:44:05 -0700

    style: add margin and padding styles to App.jsx to ensure margin and paddings are consistent across all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar.jsx';
+
 import Home from './pages/Home.jsx';
+import Wics from './pages/Wics.jsx';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={`overflow-x-hidden max-w-[128rem] mx-auto px-[5%] py-[2rem]`}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sfu-wics" element={<Wics />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
