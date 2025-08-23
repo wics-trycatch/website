fd6e79facfb4965effc213066a3f55a6130c8804
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-08-23 13:49:06 -0700

    feat: add keynote and panelists arrays to easily change and add them in the future

---
+++ src/data/panelists.js
@@ -0,0 +1,43 @@
+import panelist1 from "../assets/images/wics/placeholder_headshot.png";
+import panelist2 from "../assets/images/wics/placeholder_headshot.png";
+import panelist3 from "../assets/images/wics/placeholder_headshot.png";
+import panelist4 from "../assets/images/wics/placeholder_headshot.png";
+
+export const panelists = [
+  {
+    img: panelist1,
+    imgProperties: "object-center",
+    alt: "Panelist 1 headshot",
+    name: "Panelist 1",
+    role: "SFU Student",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: panelist2,
+    imgProperties: "object-center",
+    alt: "Panelist 2 headshot",
+    name: "Panelist 2",
+    role: "SFU Student",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: panelist3,
+    imgProperties: "object-center",
+    alt: "Panelist 3 headshot",
+    name: "Panelist 3",
+    role: "SFU Student",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+  {
+    img: panelist4,
+    imgProperties: "object-center",
+    alt: "Panelist 4 headshot",
+    name: "Panelist 4",
+    role: "SFU Student",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+  },
+];
