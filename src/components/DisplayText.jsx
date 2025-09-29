6c6adae3f08c6591c61efc7cc891b9aa73fec6be
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-28 17:44:53 -0700

    feat: add new component for SVG display text

---
+++ src/components/DisplayText.jsx
@@ -0,0 +1,28 @@
+function DisplayText({ text, x="5", y="100", fontSize }) {
+  return (
+    <svg
+      viewBox="0 0 600 150"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-labelledby="heading"
+      className="w-full h-auto"
+    >
+      <title id="heading">sfu wics</title>
+      <text
+        x={x}
+        y={y}
+        fontSize={fontSize}
+        fill="var(--color-powder-blue)"
+        stroke="var(--color-dark-blue)"
+        strokeWidth="10"
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        fontFamily="Dream MMA"
+      >
+        {text}
+      </text>
+    </svg>
+  );
+}
+
+export default DisplayText;
