3ea694460dde42dacce3d01feb9101ec65072311
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-29 13:09:02 -0700

    style: use DisplayText component for heading 1

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -3,16 +3,18 @@ import { organizers } from "../data/organizers";
 import { keynote } from "../data/keynote";
 import { panelists } from "../data/panelists";
 
+import DisplayText from "../components/DisplayText";
+
 function Speakers() {
   return (
     <div>
       <div>
         <h1
-          className={`displayText outsideStroke font-display text-[2rem] text-powder-blue`}
+          className={`text-[2rem] text-powder-blue`}
         >
-          speakers
+          <DisplayText text="speakers"/>
         </h1>
-        <p className={`mt-[1rem]`}>
+        <p className={`mt-[-0.5rem]`}>
           Hear from inspiring women in tech as they share their journeys, challenges, and advice. Our keynote speaker and panelists bring real-world insights from industry and academia.
         </p>
       </div>
