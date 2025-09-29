03532d3295b7b3f23309b9aa33fa3e035317518a
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-29 16:35:30 -0700

    chore: format code with Prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import PhotoWindow from "../components/PhotoWindow";
 import Button from "../components/Button";
 import HeroInfoBox from "../components/HeroInfoBox";
-import GridBox from "../components/GridBox"
+import GridBox from "../components/GridBox";
 
 import banner_base from "../assets/images/home/banner_base.png";
 import banner_texture from "../assets/images/home/banner_texture.png";
@@ -14,12 +14,12 @@ import logo from "../assets/images/shared/logo_full.png";
 import scroll_more from "../assets/images/home/scroll_more.svg";
 import paint_window from "../assets/images/home/paint_app_window.svg";
 import bunny from "../assets/images/home/bunny.gif";
-import scribble from "../assets/images/home/pixel_scribble.svg"
+import scribble from "../assets/images/home/pixel_scribble.svg";
 
 function Home() {
   return (
     <div className={`mx-auto`}>
-      <Navbar/>
+      <Navbar />
       {/* Hero section */}
       <div className={`relative`}>
         <div
@@ -31,7 +31,9 @@ function Home() {
             className={`w-[24rem] sm:w-[30rem] lg:w-[40rem] 2xl:w-[50rem] sm:ml-[1rem]`}
           />
           <div className={`mt-[1rem] ml-[2rem] mr-[1rem]`}>
-            <p className={`${styles.heroDesc} max-w-[18rem] sm:max-w-[24rem] lg:max-w-[28rem] 2xl:max-w-[40rem]`}>
+            <p
+              className={`${styles.heroDesc} max-w-[18rem] sm:max-w-[24rem] lg:max-w-[28rem] 2xl:max-w-[40rem]`}
+            >
               A tech conference for high school girls and non-binary students to
               learn, connect, and get inspired.
             </p>
@@ -46,7 +48,9 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className={`scale-100 origin-top ${styles.heroBanner} border-[8px] border-dark-blue`}>
+        <div
+          className={`scale-100 origin-top ${styles.heroBanner} border-[8px] border-dark-blue`}
+        >
           <PhotoWindow
             img={banner_base}
             imgWidth="w-[100%]"
@@ -57,19 +61,21 @@ function Home() {
           />
           <div className={`absolute inset-0`}>
             <PhotoWindow
-                img={banner_texture}
-                imgWidth="w-[100%]"
-                imgHeight="h-[36rem] lg:h-[40rem] 2xl:h-[56rem]"
-                imgPosition="object-[55%_15%]"
-                windowBorder="border-[0px]"
-                alt=" "
+              img={banner_texture}
+              imgWidth="w-[100%]"
+              imgHeight="h-[36rem] lg:h-[40rem] 2xl:h-[56rem]"
+              imgPosition="object-[55%_15%]"
+              windowBorder="border-[0px]"
+              alt=" "
             />
           </div>
         </div>
       </div>
 
       {/* Try/CATCH location and date box */}
-      <div className={`absolute z-100 right-[-1rem] sm:right-[1rem] lg:right-[2rem] bottom-[10rem] sm:bottom-[10%] drop-shadow-[6px_6px_0_rgba(14,41,109,0.75)] md:drop-shadow-[8px_8px_0_rgba(14,41,109,0.75)]`}>
+      <div
+        className={`absolute z-100 right-[-1rem] sm:right-[1rem] lg:right-[2rem] bottom-[10rem] sm:bottom-[10%] drop-shadow-[6px_6px_0_rgba(14,41,109,0.75)] md:drop-shadow-[8px_8px_0_rgba(14,41,109,0.75)]`}
+      >
         <HeroInfoBox />
       </div>
       {/* END Try/CATCH location and date box */}
@@ -97,23 +103,37 @@ function Home() {
       <section className={`relative mt-[3rem]`}>
         <img src={paint_window} alt=" " className={`w-full`} />
         <div className={`${styles.about} absolute top-[7rem] left-[4.5rem]`}>
-            <h2 className={`${styles.outsideStroke} ml-[1rem]`}><span className={`block`}>What is</span><span className={`block`}>Try/CATCH?</span></h2>
+          <h2 className={`${styles.outsideStroke} ml-[1rem]`}>
+            <span className={`block`}>What is</span>
+            <span className={`block`}>Try/CATCH?</span>
+          </h2>
 
-            <img src={scribble} alt=" " className={`pl-[5.5rem] mt-[0.25rem] h-[1.5rem]`}/>
+          <img
+            src={scribble}
+            alt=" "
+            className={`pl-[5.5rem] mt-[0.25rem] h-[1.5rem]`}
+          />
 
-            <p className={`${styles.aboutDesc} pl-[1rem] pr-[2rem]`}>
-              <span className={`block mt-[1rem] mb-[1rem]`}>
-                Try/CATCH (Computing and Technology Conference for Her) is a one-day event designed to spark curiosity and confidence in female and non-binary high school students in grades 8-12. 
-              </span>
+          <p className={`${styles.aboutDesc} pl-[1rem] pr-[2rem]`}>
+            <span className={`block mt-[1rem] mb-[1rem]`}>
+              Try/CATCH (Computing and Technology Conference for Her) is a
+              one-day event designed to spark curiosity and confidence in female
+              and non-binary high school students in grades 8-12.
+            </span>
 
-              <span className={`block mb-[1rem]`}>
-                The event offers interactive workshops in programming, robotics, and design, as well as a keynote, a Women in Tech panel, and opportunities to network with university students, alumnae, and industry professionals. 
-              </span>
+            <span className={`block mb-[1rem]`}>
+              The event offers interactive workshops in programming, robotics,
+              and design, as well as a keynote, a Women in Tech panel, and
+              opportunities to network with university students, alumnae, and
+              industry professionals.
+            </span>
 
-              <span className={`block`}>
-                With over 1,400 participants since 2009, Try/CATCH is more than just a conference—it's a space to explore technology in a supportive, inclusive, and empowering environment.
-              </span>
-            </p>
+            <span className={`block`}>
+              With over 1,400 participants since 2009, Try/CATCH is more than
+              just a conference—it's a space to explore technology in a
+              supportive, inclusive, and empowering environment.
+            </span>
+          </p>
         </div>
       </section>
       {/* END What is Try/CATCH? */}
@@ -121,36 +141,36 @@ function Home() {
       {/* Highlights */}
       <section className={`relative mt-[1.5rem] overflow-hidden`}>
         <GridBox width="w-full" height="h-[40rem]" />
-        <div className={`${styles.highlights} absolute top-[3.5rem] px-[1.5rem]`}>
+        <div
+          className={`${styles.highlights} absolute top-[3.5rem] px-[1.5rem]`}
+        >
           {/* Discover Tech */}
           <div>
-            <h2 className={`${styles.highlightsTitle}`}>
-              DISCOVER TECH
-            </h2>
+            <h2 className={`${styles.highlightsTitle}`}>DISCOVER TECH</h2>
             <p>
-              Explore coding, robotics, and design through hands-on workshops built for all ages and experience levels!
+              Explore coding, robotics, and design through hands-on workshops
+              built for all ages and experience levels!
             </p>
           </div>
           {/* END Discover Tech */}
 
           {/* Meet Role Models */}
           <div>
-            <h2 className={`${styles.highlightsTitle}`}>
-              MEET ROLE MODELS
-            </h2>
+            <h2 className={`${styles.highlightsTitle}`}>MEET ROLE MODELS</h2>
             <p>
-              Connect with women in tech—from students to seasoned professionals—and hear what it’s really like in computing and engineering fields.
+              Connect with women in tech—from students to seasoned
+              professionals—and hear what it’s really like in computing and
+              engineering fields.
             </p>
           </div>
           {/* END Meet Role Models */}
 
           {/* Be Inspired */}
           <div>
-            <h2 className={`${styles.highlightsTitle}`}>
-              BE INSPIRED
-            </h2>
+            <h2 className={`${styles.highlightsTitle}`}>BE INSPIRED</h2>
             <p>
-              Fuel your curiosity with keynotes, panels, and prizes. Walk away with new skills, new friends, and a glimpse into your future.
+              Fuel your curiosity with keynotes, panels, and prizes. Walk away
+              with new skills, new friends, and a glimpse into your future.
             </p>
           </div>
           {/* END Be Inspired */}
@@ -158,7 +178,11 @@ function Home() {
 
         {/* Bunny GIF */}
         <div>
-          <img src={bunny} alt=" " className={`absolute h-[17rem] bottom-[-1.45rem] left-[2.5rem]`} />
+          <img
+            src={bunny}
+            alt=" "
+            className={`absolute h-[17rem] bottom-[-1.45rem] left-[2.5rem]`}
+          />
           {/* Blue saturation overlay */}
           <div
             className="absolute inset-0"
