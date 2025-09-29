485cb534f3fcf29e4319f140071431dfa7381cfe
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-29 13:05:33 -0700

    style: change styling to look better responsively

--- src/components/DisplayText.jsx
+++ src/components/DisplayText.jsx
@@ -1,26 +1,26 @@
 import styles from "./HeroInfoBox.module.css";
 
-function DisplayText({ text, x="5", y="100", fontSize }) {
+function DisplayText({ text, x="5", y="100" }) {
   return (
     <svg
       viewBox="0 0 600 150"
       xmlns="http://www.w3.org/2000/svg"
       role="img"
       aria-labelledby="heading"
-      className="w-full h-auto"
+      className="max-h-[7.5rem]"
     >
       <title id="heading">sfu wics</title>
       <text
         x={x}
         y={y}
-        fontSize={fontSize}
+        fontSize={72}
         fill="var(--color-powder-blue)"
         stroke="var(--color-dark-blue)"
         strokeWidth="10"
         strokeLinejoin="round"
         strokeLinecap="round"
         fontFamily="Dream MMA"
-        className={`${styles.displayText} ${styles.outsideStroke}`}
+        className={`${styles.displayText} ${styles.outsideStroke} stroke-[12px] sm:stroke-[10px]`}
       >
         {text.toLowerCase()}
       </text>
