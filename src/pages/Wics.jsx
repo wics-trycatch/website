564c51dbe6fb405c50098162f75d63992a8d0dd6
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-28 17:37:16 -0700

    feat: use text component for rounded text stroke

--- src/pages/Wics.jsx
+++ src/pages/Wics.jsx
@@ -7,11 +7,31 @@ function Wics() {
   return (
     <div>
       <div>
-        <h1
-          className={`displayText outsideStroke text-[2rem] text-powder-blue`}
-        >
-          SFU WiCS
+        <h1 className="displayText outsideStroke text-[2rem] text-powder-blue">
+          <svg
+            viewBox="0 0 600 150"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-labelledby="heading"
+            className="w-full h-auto"
+          >
+            <title id="heading">sfu wics</title>
+              <text
+                x="5"
+                y="100"
+                fontSize="72"
+                fill="var(--color-powder-blue)"
+                stroke="var(--color-dark-blue)"
+                strokeWidth="10"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                fontFamily="Dream MMA"
+              >
+                sfu wics
+              </text>
+          </svg>
         </h1>
+
         <p className={`mt-[1rem]`}>
           Try/CATCH is entirely run by enthusiastic volunteers from Simon Fraser
           University Computing Science students, faculty and staff. As such,
