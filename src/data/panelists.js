2284d1189822c30b2b4afafbb9ee41361247da63
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-28 21:35:22 -0700

    chore: add names and blurbs for speakers

--- src/data/panelists.js
+++ src/data/panelists.js
@@ -8,36 +8,36 @@ export const panelists = [
     img: panelist1,
     imgProperties: "object-center",
     alt: "Panelist 1 headshot",
-    name: "Panelist 1",
-    role: "SFU Student",
+    name: "Archita",
+    role: "Panelist",
     blurb:
-      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+      "Hello Try/Catchers!! My name is Archita, and I’m a fourth-year CS student concentrating in AI/ML with a minor in Statistics. I was previously a Technical Executive at WICS, where I hosted technical events for women and non-binary students in CS. Outside of school, I’ve been fortunate learn about AI/ML through 4 industry internships and academic research at SFU, UBC, and UofT. I am mostly interested in the intersection of AI and Medicine, but recently I’ve also been exploring Operating Systems, Biomedical Engineering, and Medical Robotics. In my free time, I love going on runs, hanging out with friends, and listening to music. I’m super excited to meet you all at SFU, and I hope you have the best time at Try/Catch 2025!"
   },
   {
     img: panelist2,
     imgProperties: "object-center",
     alt: "Panelist 2 headshot",
-    name: "Panelist 2",
-    role: "SFU Student",
+    name: "Christina",
+    role: "Panelist",
     blurb:
-      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+      "Hi, I’m Christina. I’m in my fourth year in Interactive Arts and Technology at SFU, but I previously graduated from BCIT’s Computer Systems Technology program. I’ve been working in tech since 2020, contracting for and interning at startups as well as larger tech companies like Electronic Arts, SAP, and Asana. In my spare time, I like eating chicken katsu, reading fantasy books, and tending to my farm in Stardew Valley. Feel free to hit me up if you have any questions about design internships, font pairings, and everything in between."
   },
   {
     img: panelist3,
     imgProperties: "object-center",
     alt: "Panelist 3 headshot",
-    name: "Panelist 3",
-    role: "SFU Student",
+    name: "Kaitlynn",
+    role: "Panelist",
     blurb:
-      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+      "Hi everyone, I'm Kaitlynn! I’m a Software Engineer at EA and also part of a startup’s founding team. I recently graduated from SFU, where I studied Computer Science and Business. I love keeping myself busy with all sorts of hobbies, lately I’ve been especially into baking, cycling, and boxing. I’m also a big foodie, so I’m always checking out new spots to eat. If you ever need recommendations, I’ve got plenty to share! After being a part of WiCS for years and organizing Try/CATCH a few times, I'm thrilled to be back as a panelist and excited to share my journey with you!"
   },
   {
     img: panelist4,
     imgProperties: "object-center",
     alt: "Panelist 4 headshot",
-    name: "Panelist 4",
-    role: "SFU Student",
+    name: "Eunsong",
+    role: "Panelist",
     blurb:
-      "Lorem ipsum dolor sit amet consectetur. Fames ultricies ornare augue arcu fringilla rhoncus. In donec ut magna sed massa risus eget nulla. Massa neque nibh arcu in. Praesent eu aliquet non pharetra lectus lectus. Fermentum eget aliquam habitasse enim ornare condimentum. Bibendum cursus sed eros malesuada. In fermentum in et sit odio varius ornare nunc. Urna cras tempus faucibus malesuada et facilisis fringilla. Vitae congue eget sit feugiat in vitae imperdiet maecenas euismod. Amet."
+      "Hey there! I’m Song, a 4th year Computer Science student at SFU. Throughout my time here, I’ve explored different areas of CS- from interning as a fullstack developer, to researching security vulnerabilities with LLMs at SFU’s Reliable Systems Lab, to my current role as a Software Engineer Intern working on AI/ML R&D at a cybersecurity startup. In my first year, I founded the Developers and Systems Club, which hosts industry talks, workshops, and hackathons for programmers of all levels, and has since grown to over 400 members! With WiCS, I’ve hosted fullstack workshops and taken part in the mentorship program as both a mentee and mentor. Outside of school, I enjoy hackathons, coding, DJing, and going to concerts. I’m so excited to be at Try/CATCH this year and can’t wait to meet everyone <3"
   },
 ];
