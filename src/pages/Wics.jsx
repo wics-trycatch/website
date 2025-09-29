07c97bbd63ce06b229bba8020c4e1f12da44009c
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-28 17:45:04 -0700

    feat: use new display text component

--- src/pages/Wics.jsx
+++ src/pages/Wics.jsx
@@ -1,6 +1,7 @@
 import styles from "./Wics.module.css";
 
 import ProfileCard from "../components/ProfileCard";
+import DisplayText from "../components/DisplayText";
 import { organizers } from "../data/organizers";
 
 function Wics() {
@@ -8,28 +9,7 @@ function Wics() {
     <div>
       <div>
         <h1 className="displayText outsideStroke text-[2rem] text-powder-blue">
-          <svg
-            viewBox="0 0 600 150"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-labelledby="heading"
-            className="w-full h-auto"
-          >
-            <title id="heading">sfu wics</title>
-              <text
-                x="5"
-                y="100"
-                fontSize="72"
-                fill="var(--color-powder-blue)"
-                stroke="var(--color-dark-blue)"
-                strokeWidth="10"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                fontFamily="Dream MMA"
-              >
-                sfu wics
-              </text>
-          </svg>
+          <DisplayText text="sfu wics" fontSize="72" />
         </h1>
 
         <p className={`mt-[1rem]`}>
