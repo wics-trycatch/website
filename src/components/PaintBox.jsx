e04a438ec807a41a552dab7a47ebeda555b564f8
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-29 19:17:20 -0700

    feat: add responsive paint application background

---
+++ src/components/PaintBox.jsx
@@ -0,0 +1,109 @@
+import styles from "../pages/Home.module.css";
+
+import window_options from "../assets/images/shared/window_options_complex.svg";
+import window_name from "../assets/images/shared/info_bmp.svg";
+import paint_tools_double from "../assets/images/shared/paint_tools_double.png";
+import paint_tools_single from "../assets/images/shared/paint_tools_single.png";
+import paint_nav from "../assets/images/shared/window_tools.png"
+import scribble from "../assets/images/home/pixel_scribble.svg";
+
+function PaintBox() {
+  return (
+    <section
+      className={`relative mt-[3rem] p-[1rem]`}
+      style={{
+        boxShadow:
+          "inset 0 -5px 0 0 var(--color-sky-blue), inset -5px 0 0 0 var(--color-sky-blue), inset 0 5px 0 0 white, inset 5px 0 0 0 white",
+      }}
+    >
+      <div className={`w-full border-b-[3rem] border-dark-blue`}>
+          <img src={window_name} alt=" " className={`absolute h-[1.5rem] sm:h-[2rem] top-[1.75rem] sm:top-[1.5rem] left-[2rem] sm:left-[1.5rem]`} />
+          <img src={window_options} alt=" " className={`absolute h-[1.5rem] :h-[2rem] top-[1.75rem] sm:top-[1.5rem] right-[2rem] sm:right-[1.5rem]`} />
+          <img src={paint_nav} alt=" " className={`absolute h-[1.75rem] sm:h-[2.25rem] md:h-[1.75rem] top-[4.25rem] left-[1.25rem] sm:left-[1.75rem]`} />
+          <img src={paint_tools_single} alt=" " className={`absolute w-[2.75rem] top-[7rem] left-[1.25rem]`} />
+          {/* <img src={paint_tools_double} alt=" " className={`absolute w-[5rem] hidden sm:block sm:w-[8rem] top-[7rem] sm:top-[9rem] left-[1.25rem] sm:left-[1.75rem]`} /> */}
+      </div>
+
+    
+      <div className={`relative pl-[3.5rem]`}>
+        <div className={`${styles.about} mt-[3rem] pb-[2rem] min-h-[30.3rem] overflow-x-hidden bg-white border-[3px] border-black`}>
+          <h2 className={`mt-[1.5rem] ml-[0.75rem] sm:ml-[1.5rem] md:ml-[0.75rem]`}>
+            <svg
+              viewBox="0 0 500 150"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-labelledby="heading"
+              className="h-[3.5rem] sm:h-[5rem] md:h-[3.5rem] mt-[-1.25rem] ml-[1rem] mb-[-2rem] sm:mb-[-3rem] md:mb-[-2rem]"
+            >
+              <title id="heading">about</title>
+              <text
+                x="8"
+                y="100"
+                fontSize={72}
+                fill="white"
+                stroke="black"
+                strokeWidth="10"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                fontFamily="Dream MMA"
+                className={`${styles.displayText} ${styles.outsideStroke} stroke-[20px]`}
+              >
+                about
+              </text>
+            </svg>
+            <svg
+              viewBox="0 0 600 150"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-labelledby="heading"
+              className="h-[3.5rem] sm:h-[5rem] md:h-[3.5rem] mb-[0.5rem] lg:mb-[-0.5rem] md:mb-[0.5rem]"
+            >
+              <title id="heading">try/catch</title>
+              <text
+                x="8"
+                y="100"
+                fontSize={72}
+                fill="white"
+                stroke="black"
+                strokeWidth="10"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                fontFamily="Dream MMA"
+                className={`${styles.displayText} ${styles.outsideStroke} stroke-[20px]`}
+              >
+                try/catch
+              </text>
+            </svg>
+          </h2>
+          <img
+            src={scribble}
+            alt=" "
+            className={`pl-[3.5rem] sm:pl-[6rem] md:pl-[3.5rem] mt-[-1rem] h-[1.5rem] sm:h-[2rem] md:h-[1.5rem]`}
+          />
+          <p
+            className={`${styles.aboutDesc} mt-[1rem] pl-[1rem] sm:pl-[2rem] md:pl-[1rem] pr-[0.75rem] sm:pr-[2rem] md:pr-[0.75rem] h-[77%]`}
+          >
+            <span className={`block mb-[1rem]`}>
+              Try/CATCH (Computing and Technology Conference for Her) is a one-day
+              event designed to spark curiosity and confidence in female and
+              non-binary high school students in grades 8-12.
+            </span>
+            <span className={`block mb-[1rem]`}>
+              The event offers interactive workshops in programming, robotics, and
+              design, as well as a keynote, a Women in Tech panel, and
+              opportunities to network with university students, alumnae, and
+              industry professionals.
+            </span>
+            <span className={`block`}>
+              With over 1,400 participants since 2009, Try/CATCH is more than just
+              a conference—it's a space to explore technology in a supportive,
+              inclusive, and empowering environment.
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PaintBox;
