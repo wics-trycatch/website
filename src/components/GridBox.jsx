e14e53116d7bc61d12c22388eaa98c7f387d2d73
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-30 15:38:45 -0700

    style: add window option box

--- src/components/GridBox.jsx
+++ src/components/GridBox.jsx
@@ -1,10 +1,12 @@
 import GridLines from "react-gridlines";
+import window_options from "../assets/images/shared/window_options_complex.svg";
 
 function GridBox({width, height}) {
   return (
     <div
-      className={`relative ${width} ${height} bg-white border-[4px] border-t-[32px] border-dark-blue overflow-hidden`}
+      className={`relative ${width} ${height} bg-white border-[4px] border-t-[40px] border-dark-blue`}
     >
+      <img src={window_options} alt=" " className={`absolute h-[1.5rem] sm:h-[1.875rem] top-[-1.95rem] sm:top-[-2.1rem] right-[0.5rem] sm:right-[0.5rem]`} />
       <GridLines
         className="absolute inset-0 p-[1rem]"
         cellWidth={40}
