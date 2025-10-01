dd19bae2a0c3ec90499d9830bef5004c80047723
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-09-30 22:01:01 -0700

    chore: add data file for sponsors

---
+++ src/data/sponsors.js
@@ -0,0 +1,22 @@
+export const sponsors = [
+    {
+        name: "Gold Company",
+        logo: "/path/to/logo.png",
+        blurb: "Description of the sponsor and their contribution...",
+        website: "https://company.com",
+        tier: "gold"
+    },
+    {
+        name: "Silver Company",
+        logo: "/path/to/logo.png",
+        website: "https://company.com",
+        tier: "silver"
+    },
+    {
+        name: "Bronze Company",
+        logo: "/path/to/logo.png",
+        website: "https://company.com",
+        tier: "bronze"
+    }
+
+]
