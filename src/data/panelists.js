2e52df987cf0f88525b0d13481cc4f3a613b6550
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-10-02 18:56:07 -0700

    fix: swap variable names for christina and kaitlynn

--- src/data/panelists.js
+++ src/data/panelists.js
@@ -1,11 +1,11 @@
-import panelist1 from "../assets/images/speakers/panelists/archita.png";
-import panelist2 from "../assets/images/speakers/panelists/kaitlynn.jpg";
-import panelist3 from "../assets/images/speakers/panelists/christina.png";
-import panelist4 from "../assets/images/speakers/panelists/eunsong.png";
+import archita from "../assets/images/speakers/panelists/archita.png";
+import christina from "../assets/images/speakers/panelists/christina.png";
+import kaitlynn from "../assets/images/speakers/panelists/kaitlynn.jpg";
+import eunsong from "../assets/images/speakers/panelists/eunsong.png";
 
 export const panelists = [
   {
-    img: panelist1,
+    img: archita,
     imgProperties: "object-center",
     alt: "Panelist 1 headshot",
     name: "Archita",
@@ -14,7 +14,7 @@ export const panelists = [
       "Hello Try/Catchers!! My name is Archita, and I’m a fourth-year CS student concentrating in AI/ML with a minor in Statistics. I was previously a Technical Executive at WICS, where I hosted technical events for women and non-binary students in CS. Outside of school, I’ve been fortunate learn about AI/ML through 4 industry internships and academic research at SFU, UBC, and UofT. I am mostly interested in the intersection of AI and Medicine, but recently I’ve also been exploring Operating Systems, Biomedical Engineering, and Medical Robotics. In my free time, I love going on runs, hanging out with friends, and listening to music. I’m super excited to meet you all at SFU, and I hope you have the best time at Try/Catch 2025!"
   },
   {
-    img: panelist2,
+    img: christina,
     imgProperties: "object-center",
     alt: "Panelist 2 headshot",
     name: "Christina",
@@ -23,7 +23,7 @@ export const panelists = [
       "Hi, I’m Christina. I’m in my fourth year in Interactive Arts and Technology at SFU, but I previously graduated from BCIT’s Computer Systems Technology program. I’ve been working in tech since 2020, contracting for and interning at startups as well as larger tech companies like Electronic Arts, SAP, and Asana. In my spare time, I like eating chicken katsu, reading fantasy books, and tending to my farm in Stardew Valley. Feel free to hit me up if you have any questions about design internships, font pairings, and everything in between."
   },
   {
-    img: panelist3,
+    img: kaitlynn,
     imgProperties: "object-center",
     alt: "Panelist 3 headshot",
     name: "Kaitlynn",
@@ -32,7 +32,7 @@ export const panelists = [
       "Hi everyone, I'm Kaitlynn! I’m a Software Engineer at EA and also part of a startup’s founding team. I recently graduated from SFU, where I studied Computer Science and Business. I love keeping myself busy with all sorts of hobbies, lately I’ve been especially into baking, cycling, and boxing. I’m also a big foodie, so I’m always checking out new spots to eat. If you ever need recommendations, I’ve got plenty to share! After being a part of WiCS for years and organizing Try/CATCH a few times, I'm thrilled to be back as a panelist and excited to share my journey with you!"
   },
   {
-    img: panelist4,
+    img: eunsong,
     imgProperties: "object-center",
     alt: "Panelist 4 headshot",
     name: "Eunsong",
